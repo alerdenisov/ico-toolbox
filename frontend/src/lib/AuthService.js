0199781404b52f498415d690efcf975e9911bfeb
Aler Denisov <aler.zampillo@gmail.com>
2018-01-11 17:40:45 +0300

    single time code

--- frontend/src/lib/AuthService.js
+++ frontend/src/lib/AuthService.js
@@ -1,8 +1,21 @@
 import Auth0Lock from 'auth0-lock'
+import Auth0LockPasswordless from 'auth0-lock-passwordless'
 import eventEmitter from 'event-emitter'
 
 export default class AuthService {
   constructor () {
+    this.auth0passwordless = new Auth0LockPasswordless(
+      'fSCBGh15rlKaKyhzkrfq1w2tROQibzrE',
+      'aofg.eu.auth0.com',
+      {
+        auth: {
+          params: {
+            scope: 'openid profile name email picture'
+          }
+        }
+      }
+    )
+
     this.auth0 = new Auth0Lock(
       'fSCBGh15rlKaKyhzkrfq1w2tROQibzrE',
       'aofg.eu.auth0.com',
@@ -35,7 +48,21 @@ export default class AuthService {
   }
 
   show () {
-    this.auth0.show()
+    // this.auth0passwordless.emailcode((err, profile, idToken, state) => {
+    //   console.log(err, profile, idToken, state)
+    // })
+
+    // this.auth0passwordless.magiclink((err, profile, id_token) => {
+    //   console.log(err, profile, id_token)
+    //   // Passwordless Authentication handler
+    // });
+    this.auth0passwordless.emailcode({
+      callbackURL: 'http://localhost:8080/',
+      authParams: {
+        scope: 'openid profile name email picture'  // Learn about scopes: https://auth0.com/docs/scopes
+      }
+    })
+    // this.auth0.show()
   }
 
   hide () {
