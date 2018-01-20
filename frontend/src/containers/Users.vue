c4e3417ce153c750da57f095a1db14d581d15535
Aler Denisov <aler.zampillo@gmail.com>
2018-01-20 15:29:00 +0300

    User list draft

--- frontend/src/containers/Users.vue
+++ frontend/src/containers/Users.vue
@@ -1,8 +1,26 @@
 <template lang="pug">
   div(:class='b()' ref='container')
     el-card(:class="b('card')")
-      el-table(:data='users' style='width: 100%' :max-height="maxHeight" :class='b("table")' ref='table')
+      el-button(@click='createVisible = !createVisible') Create new
+
+      el-table(:data='users' style='width: 100%; margin-top: 30px' :max-height="maxHeight" :class='b("table")' ref='table')
         el-table-column(prop='_id' label='ID')
+        el-table-column(prop='nickname' label='Display')
+        el-table-column(prop='email' label='Email')
+        el-table-column(prop='refId' label='Ref ID')
+        el-table-column(prop='roles' label='Roles')
+        el-table-column(prop='referrer' label='Referrer')
+
+      el-dialog(
+        title="Tips"
+        :visible.sync="createVisible"
+        width="30%"
+        :before-close="handleClose")
+        span This is a message
+        span.dialog-footer(slot='footer')
+          el-button(@click='createVisible = false') Cancel
+          el-button(type='primary', @click='createVisible = false') Confirm
+
 </template>
 
 <script>
@@ -15,6 +33,7 @@ export default {
   },
   data () {
     return {
+      createVisible: false,
       showType: 'your',
       maxHeight: 200,
       resizeTimeout: null
